3ae62110a7f869768daa5fb7d8945828f092c568
DinethKeragala <dinethkeragala@gmail.com>
2024-12-25 19:55:38 +0530

    1

--- donations-management-backend/ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ donations-management-backend/ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6566ca73a306c04b8866e0630cd5e68fc97e7f0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17377b8ac78818c03d1a7bbfb121e9f69878b6f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
