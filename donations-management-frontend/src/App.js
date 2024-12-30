117a405d7e030561f2f040d2df26f4c10328adc7
DinethKeragala <dinethkeragala@gmail.com>
2024-12-30 11:01:29 +0530

    Login and SignUp

--- donations-management-frontend/src/App.js
+++ donations-management-frontend/src/App.js
@@ -1,46 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar'; 
-import HomePage from './pages/HomePage';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
-import SignUpPage from './pages/SignUpPage';
-
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import SignupPage from './pages/SignUpPage';
 
 const App = () => {
   return (
-      <Router>
-          <Navbar />
-          <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/signup" element={<SignUpPage />} />
-          </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/" element={<Navigate to="/login" />} /> {/* Redirect to login */}
+      </Routes>
+    </Router>
   );
 };
-
 
 export default App;
