ad8321643fee8f10e59539fb87e5fcb795ec2601
DinethKeragala <dinethkeragala@gmail.com>
2025-01-05 10:55:10 +0530

    navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,20 +1,34 @@
 import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 import { FaBars, FaTimes } from 'react-icons/fa';
+import { Button } from './Button';
+import './Navbar.css'; 
 
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true)
+
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960){
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+
+  window.addEventListener('resize', showButton);
+
+  }
   return (
     <>
         <nav className='navbar'>
-            <div className='navbar-container'>
+          <div className='navbar-container'>
                 <Link to='/' className='navbar-logo'>
                 HealTheWorld
                 </Link>
-            </div>
             <div className='menu-icon' onClick={handleClick}>
                 {click ? <FaTimes /> : <FaBars />}
             </div>
@@ -40,6 +54,9 @@ function Navbar() {
                 </Link>
               </li>
             </ul>
+            {button && <Button buttonStyle='btn--outline'>
+              SIGN UP</Button>}
+          </div>
         </nav>
     </>
   )
