74a0f9df7dd2e4c9e47608feda9c20c51f3f9f79
DinethKeragala <dinethkeragala@gmail.com>
2025-03-06 03:06:24 +0530

    blah

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../App.css';
 import ProfilePage from '../components/ProfilePage';
+import Footer from '../components/Footer';
 
 function Profile() {
   return (
     <>
       <ProfilePage />
+      <Footer />
     </>
   )
 }
